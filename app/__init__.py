5415f6b1e229f24f5127b8b8463c644814d75835
Wainggan <wainggan@gmail.com>
2023-09-12 19:31:33 -0700

    clean up

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 
 from flask import Flask
 from config import Config
+from instance.config import InstanceConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -8,6 +9,7 @@ from flaskext.markdown import Markdown
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config.from_object(InstanceConfig)
 
 login = LoginManager(app)
 
---
+++ instance/instanceconfigtemplate.py
@@ -0,0 +1,10 @@
+
+# duplicate and rename this to "config.py"
+
+from config import Config
+
+class InstanceConfig (Config):
+	SECRET_KEY = ""
+	SQLALCHEMY_DATABASE_URI = ""
+
+
