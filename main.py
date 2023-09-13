44389e7a94a14026c536dbef14afe3875b7e1e3f
parchii <wainggan@gmail.com>
2023-09-12 23:05:32 -0700

    stuff

--- main.py
+++ main.py
@@ -2,5 +2,6 @@
 from app import app
 
 if __name__ == '__main__':
-	app.run('localhost', debug=True, ssl_context=('cert.pem', 'key.pem'))
+	#app.run('localhost', debug=True, ssl_context=('cert.pem', 'key.pem'))
+	app.run('localhost', debug=True)
 
